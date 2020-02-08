b157b0a2e54f8c363a29f3b125690446ef3ad32a
AdamMC-GL <mahmoudchebil@live.nl>
2020-02-08 11:49:34 +0100

    Update opdracht12-AI.py

--- opdracht12-AI.py
+++ opdracht12-AI.py
@@ -1,7 +1,11 @@
 
 for numbers in range(1, 101, 1):
     output = ''
-    if (numbers % 3) == 0: output += 'Fizz'
-    if (numbers % 5) == 0: output += 'Buzz'
-    if output == '': output = numbers
+    if (numbers % 3) == 0:
+        output += 'Fizz'
+    if (numbers % 5) == 0:
+        output += 'Buzz'
+
+    if output == '':
+        output = numbers
     print(output)
